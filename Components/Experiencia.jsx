2447b72fc218a2e3196d3c5763dbb0aebf60789f
julihumere <julihumere@gmail.com>
2025-02-03 13:22:45 -0300

    Refactor styles for SobreMi, MenuDesplegable, and Experiencia components; enhance responsiveness and layout, and update language files with new section titles and descriptions.

--- Components/Experiencia.jsx
+++ Components/Experiencia.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../styles/Experiencia.module.css";
+import Image from "next/image";
 
 export default function Experiencia({ props }) {
   return (
@@ -10,11 +11,84 @@ export default function Experiencia({ props }) {
           <h1>{props.nombre}</h1>
           <h2>{props.fecha}</h2>
         </div>
-        <h4 className={styles.item__text}>{props.text}</h4>
+        <p className={styles.item__text}>{props.text}</p>
 
-        <h5 className={styles.item__skills}>
-          Habilidades técnicas: {props.Habilidades}
-        </h5>
+        <div className={styles.item__skills}>
+          <strong>{props.titulohab}</strong>
+          <ul>
+            <li>
+              <Image
+                alt="Javascript"
+                src="/img/FrontEnd/Javascript.png"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>Javascript</p>
+            </li>
+            <li>
+              <Image
+                alt="React Native"
+                src="/img/FrontEnd/ReactNative.webp"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>React Native</p>
+            </li>
+            <li>
+              <Image
+                alt="React"
+                src="/img/FrontEnd/React.png"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>React</p>
+            </li>
+
+            <li>
+              <Image
+                alt="PHP"
+                src="/img/FrontEnd/PHP.webp"
+                width={70}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>PHP</p>
+            </li>
+            <li>
+              <Image
+                alt="Node JS"
+                src="/img/Backend/nodejs.png"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>Node JS</p>
+            </li>
+            <li>
+              <Image
+                alt="Express"
+                src="/img/Backend/express.png"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>Express</p>
+            </li>
+            <li>
+              <Image
+                alt="SQL"
+                src="/img/Backend/sql.png"
+                width={50}
+                height={50}
+                className={styles.img__skills}
+              />
+              <p>MySql</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
